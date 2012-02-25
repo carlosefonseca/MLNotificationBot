d9a232ad2c31fa171d4aa2b50a47e64216f5cdbb
Carlos Fonseca <carlosefonseca@gmail.com>
2012-02-25 00:23:27 +0000

    alterada a lib de twitter. Melhorado o handling de texto para os tweets

--- metro.php
+++ metro.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_COMPILE_ERROR);
+
 // Setup
 $debug=false;
 $lastChangeFPath = "last.html";
@@ -11,15 +13,14 @@ $oldpage = loadLocalData($lastChangeFPath);
 $page = downloadPage($url);
 
 // EXIT IF NO CHANGE
-if (strcmp($page, $oldpage) == 0) die("");
+if (strcmp($page, $oldpage) == 0) die("#");
+echo "\n";
 
 // SAVE NEW PAGE
 file_put_contents($lastChangeFPath, $page);
 
 // EXTRACT CHANGED DATA
 $changedData = findChanges($page, $oldpage);
-
-// print_r($changedData);
 
 // ACT ON CHANGED DATA
 foreach($changedData as $data) {
@@ -30,10 +31,18 @@ die("\n");
 
 
 //////// FUNCTIONS ////////
-
 function actOnChangedData($data) {
-    $text = strtoupper($data["line"]).":\n".$data["descr"];
+    $text = strtoupper($data["line"]).": ".$data["descr"];
+    $sorry = strpos($text," Pedimos desculpa pelo i");
+    if ($sorry != -1) {
+        $text = substr($text, 0, $sorry);
+    }
+    $text = substr($text, 0, 140);
+
+    $text = utf8_encode(html_entity_decode($text));
     echo "New Tweet: ".$text;
+
+    echo " Twitter: ".post_tweet($text);
 }
 
 
@@ -54,21 +63,15 @@ function downloadPage($url) {
     global $debug;
     global $template;
 	
-	if ($debug) {
-	    return loadLocalData("example.html");
-	}
-	
 	$ch = curl_init($url);
 
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_REFERER, "http://www.metrolisboa.pt/");
+	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.18.5 (KHTML, like Gecko) Version/5.2 Safari/535.18.5");
 
 	$f = curl_exec($ch);
 	curl_close($ch);
-	
-	$fp = fopen("example.html","w+");
-    fwrite($fp, $f);
-    fclose($fp);
 	
 	return $f;
 }
@@ -77,25 +80,45 @@ function downloadPage($url) {
 function findChanges($text, $oldtxt) {
     // OLD
 	$first = strpos($oldtxt, "<tr>");
-    $a = preg_match_all("|\<tr>\<td[^>]+>\<b>([^\<]+)\</b>\</td>\<td[^>]*>\s*\<ul class=\"([^\"]*)\">(\<li>(.*)\</li>)+\</ul>|", $oldtxt, $matches, PREG_SET_ORDER, $first);
+    $a = preg_match_all("|\<tr>\<td[^>]+>\<b>([^\<]+)\</b>\</td>\<td[^>]*>\s*\<ul[^>]+>(\<li>(.*)\</li>)+\</ul>|", $oldtxt, $matches, PREG_SET_ORDER, $first);
     $old = array();
     foreach($matches as $v) {
-        $descr = preg_replace("|\</li>\s*<li[^>]*>|", "; ", $v[4]);
-        // echo $v[4]."\n—— ".$descr."\n\n\n";
-        $old[] = array("line"=>$v[1] , "state" => $v[2] , "descr" => $descr);
+        $descr = preg_replace("|\</li>\s*<li[^>]*>|", "; ", $v[3]);
+        $old[] = array("line"=>$v[1] , "descr" => $descr);
     }
 
     // NEW
 	$first = strpos($text, "<tr>");
-    $a = preg_match_all("|\<tr>\<td[^>]+>\<b>([^\<]+)\</b>\</td>\<td[^>]*>\s*\<ul class=\"([^\"]*)\">(\<li>(.*)\</li>)+\</ul>|", $text, $matches, PREG_SET_ORDER, $first);
+    $a = preg_match_all("|\<tr>\<td[^>]+>\<b>([^\<]+)\</b>\</td>\<td[^>]*>\s*\<ul[^>]+>(\<li>(.*)\</li>)+\</ul>|", $text, $matches, PREG_SET_ORDER, $first);
     $changes = array();
     foreach($matches as $k=>$v) {
-        $descr = preg_replace("|\</li>\s*<li[^>]*>|", "; ", $v[4]);
+        $descr = preg_replace("|\</li>\s*<li[^>]*>|", "; ", $v[3]);
         if (strcmp($descr,$old[$k]["descr"]) != 0) {
-            $changes[] = array("line"=>$v[1] , "state" => $v[2] , "descr" => $descr);
+            $changes[] = array("line"=>$v[1] , "descr" => $descr);
         }
     }
-    
-    // print_r($output);
     return $changes;
+}
+
+function post_tweet($tweet_text) {
+    require 'tmhOAuth/tmhOAuth.php';
+    require 'tmhOAuth/tmhUtilities.php';
+    $tmhOAuth = new tmhOAuth(array(
+      'consumer_key'    => '',
+      'consumer_secret' => '',
+      'user_token'      => '',
+      'user_secret'     => '',
+    ));
+
+    $code = $tmhOAuth->request('POST', $tmhOAuth->url('1/statuses/update'), array(
+      'status' => $tweet_text
+    ));
+
+    if ($code == 200) {
+        $tmhOAuth->response['response'];
+        return "OK";
+      // tmhUtilities::pr(json_decode($tmhOAuth->response['response']));
+    } else {
+      tmhUtilities::pr($tmhOAuth->response['response']);
+    }
 }
